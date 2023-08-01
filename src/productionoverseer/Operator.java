6b879b8e1cb6c4809084600e159a100e54433f9a
Alex Jansen <alexjansen210@gmail.com>
2023-08-01 15:16:42 -0700

    Remove HAP share letter & args prep

--- src/productionoverseer/Operator.java
+++ src/productionoverseer/Operator.java
@@ -12,6 +12,8 @@ import productionoverseer.EIMMTLink.FoundDuplicateOrderException;
 public class Operator {
 
 	public static void main(String... args) {
+		for (String arg : args) System.out.println(arg);
+		
 		LocalDate today = LocalDate.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddyyyy");
 
@@ -19,13 +21,13 @@ public class Operator {
 		String dateTo = null;
 		String ordDeskUser = "3605982";
 		String excelDest = "C:/temp/" + dateFrom + ".xlsx";
-		String hapShareLetter = "T:/";
+		String hapShare = "//Mw/wch-mil/PEDS_HAP_SHARE/";
 
 		List<String> roots = List
 				.of("Auburn/Retained", "Auburn/Request", "Auburn/CGM", "Auburn/TIFF", "Everett/Retained",
 						"Everett/Request", "Everett/CGM", "Everett/TIFF", "St_Louis/Retained", "St_Louis/Request",
 						"St_Louis/CGM", "St_Louis/TIFF")
-				.parallelStream().map(dir -> hapShareLetter + dir).collect(Collectors.toList());
+				.parallelStream().map(dir -> hapShare + dir).collect(Collectors.toList());
 
 		EIMMTLink eimmtLink = new EIMMTLink();
 
