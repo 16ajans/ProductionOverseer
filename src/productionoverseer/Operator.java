08a1a816c089eef08106505a8ec48c94a7c825c2
Alex Jansen <alexjansen210@gmail.com>
2023-08-10 12:53:37 -0700

    Handle filename collisions

--- src/productionoverseer/Operator.java
+++ src/productionoverseer/Operator.java
@@ -1,10 +1,13 @@
 package productionoverseer;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import productionoverseer.EIMMTLink.FoundDuplicateOrderException;
@@ -41,9 +44,9 @@ public class Operator {
 			}
 		}
 
-		String excelDest = outputDir + "/" + dateFrom
+		File excelDest = getUniqueFileName(new File(outputDir + "/" + dateFrom
 				+ (dateFrom.equals(today) ? "" : "_THRU_" + (dateTo != null ? dateTo : today))
-				+ (ordDeskUser != null ? "_FOR_" + ordDeskUser : "") + ".xlsx";
+				+ (ordDeskUser != null ? "_FOR_" + ordDeskUser : "") + ".xlsx"));
 		String hapShare = "//Mw/wch-mil/PEDS_HAP_SHARE/";
 
 		List<String> roots = List
@@ -105,19 +108,53 @@ public class Operator {
 				.map(order -> new BundledOrder(order, searchResults, hapShare)).collect(Collectors.toList());
 
 		try {
-			ExcelLink.export(excelDest, bundledOrders, null);
+			ExcelLink.export(excelDest.toString(), bundledOrders, null);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		try {
 			Runtime.getRuntime().exec(
-					"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -Command \"" + excelDest + "\"");
+					"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -Command \"& '" + excelDest.toString() + "'\"");
 			System.out.println("Opening report . . .");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	private static File getUniqueFileName(File file) {
+	    File originalFile = file;
+	    try {
+	        while (file.exists()) {
+	            String newFileName = file.getName();
+	            String baseName = newFileName.substring(0, newFileName.lastIndexOf("."));
+	            String extension = getExtension(newFileName);
+
+	            Pattern pattern = Pattern.compile("( \\(\\d+\\))\\."); // Find ' (###).' in the file name, if it exists
+	            Matcher matcher = pattern.matcher(newFileName);
+
+	            String strDigits = "";
+	            if (matcher.find()) {
+	                baseName = baseName.substring(0, matcher.start(0)); // Remove the (###)
+	                strDigits = matcher.group(0); // Grab the ### we'll want to increment
+	                strDigits = strDigits.substring(strDigits.indexOf("(") + 1, strDigits.lastIndexOf(")")); // Strip off the ' (' and ').' from the match
+	                // Increment the found digit and convert it back to a string
+	                int count = Integer.parseInt(strDigits);
+	                strDigits = Integer.toString(count + 1);
+	            } else {
+	                strDigits = "1"; // If there is no (###) match then start with 1
+	            }
+	            file = new File(file.getParent() + "/" + baseName + " (" + strDigits + ")" + extension); // Put the pieces back together
+	        }
+	        return file;
+	    } catch (Error e) {
+	        return originalFile; // Just overwrite the original file at this point...
+	    }
+	}
+
+	private static String getExtension(String name) {
+	    return name.substring(name.lastIndexOf("."));
 	}
 
 }
